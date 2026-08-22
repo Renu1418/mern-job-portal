61af3ac99102b433e88c6afeb665ee9572dce540
Renu1418 <renusharma1418@gmail.com>
2026-08-22 10:00:51 +0530

    fix: update cors origin with exact live url

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 
 // fromtemd-backend cors configuration
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://mern-job-portal-frontend.onrender.com"],
+  origin: ["http://localhost:5173", "https://mern-job-portal-frontend-piwr.onrender.com"],
   credentials: true,
 };
 app.use(cors(corsOptions));
